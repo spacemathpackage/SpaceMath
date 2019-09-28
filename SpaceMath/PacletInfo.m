f25fe6b6360485ddab3ae516efb45ac0056d825c
valencia <valencia@adrii>
2019-09-28 12:46:56 -0500

    Update

---
+++ SpaceMath/PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2019.09.28*)
+
+Paclet[
+  Name -> "SpaceMath",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
---
+++ SpaceMath/SpaceMath/SpaceMath.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench Sep 28, 2019 *)
+
+BeginPackage["SpaceMath`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
